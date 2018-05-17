eca7bd560e2b58e0b377d0fa8f23ba5a49fd68dd
rachelgrau <rgrau@stanford.edu>
2018-05-17 13:22:20 -0300

    Initial Commit

---
+++ ToDoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoApp
+//
+//  Created by Rachel on 17/05/2018.
+//  Copyright © 2018 Rachel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
